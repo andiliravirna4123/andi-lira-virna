391787aea45c8585b148741d3142e1400c0546cc
andiliravirna4123 <andiliravirna4123@gmail.com>
2026-01-05 22:16:22 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Belajar API PHP</title>
-</head>
-<body>
-    <h1>Message Form</h1>  
-    <form action="submit.php" method="POST">
-         <label>Nama :</label>
-         <input type="text" name="nama"> <br>
-         
-         <label>Nama :</label>
-         <textarea name="pesan"></textarea> <br>
-         
-         <button type="submit">submit</button>
-    </form>    
-</body>
-</html>
+<?php
+phpinfo();
+?>
